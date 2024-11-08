2d75079656c9921cee53d80b807483e59abcf7b2
Max Koste <maxkoste@gmail.com>
2024-11-08 19:03:24 +0100

    basic navigation

--- src/components/MainDisplay/MainDisplay.tsx
+++ src/components/MainDisplay/MainDisplay.tsx
@@ -1,11 +1,20 @@
 import { Box } from "@mui/material";
+import Content from "../Content/Content";
+import { useState, ReactNode } from "react";
+import Home from "../Pages/Home";
+import Navigation from "../Navigation/Navigation";
 
 function MainDisplay() {
-	return (
-		<Box>
-			WIP SITE
-		</Box>
-	)
+  // Set initial state to be of type ReactNode
+  const [renderedContent, setRenderedContent] = useState<ReactNode>(<Home />);
+
+  return (
+    <Box>
+      <Navigation setRenderedContent={setRenderedContent} />
+		
+      <Content renderedContent={renderedContent} />
+    </Box>
+  );
 }
 
-export default MainDisplay
+export default MainDisplay;
