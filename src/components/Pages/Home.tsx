2a17d149bfd985109a8ff51657e8ef18e4a3db48
Max Koste <maxkoste@gmail.com>
2025-02-10 23:10:53 +0100

    changed playbutton on homepage

--- src/components/Pages/Home.tsx
+++ src/components/Pages/Home.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <p className="bg-white bg-clip-text text-4xl font-mono font-extrabold text-transparent border-4 p-4">
+    <div className="flex flex-col justify-center h-screen">
+      <p className="bg-white bg-clip-text text-4xl text-center font-mono font-extrabold text-transparent border-4 p-4">
         EP 'DERECHO' OUT NOW
       </p>
       <div className="mt-12 flex flex-col items-center">
         <button
-          className="group relative w-20 h-20 rounded-full bg-gray-400/70 flex items-center 
+          className="group relative w-20 h-20 rounded-full bg-white/70 flex items-center 
           justify-center overflow-hidden hover:scale-110 transition-transform duration-300"
           onClick={() =>
             window.open(
@@ -25,7 +25,7 @@ const Home = () => {
           />
           <svg
             className="relative w-10 h-10 text-white"
-            fill="currentColor"
+            fill="gray-900"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
